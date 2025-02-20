3bcd46a86828047655961375f7c8a50c9962b292
Tarmo Lehto <tarmoejlehto@gmail.com>
2025-02-20 19:34:49 +0200

    Removed trmporary AnimatedStripes component

--- src/main.tsx
+++ src/main.tsx
@@ -1,28 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-//import App from './App.tsx'
 import { StyledEngineProvider } from '@mui/material/styles';
 import PulsePieChart from './mui/PulsePieChart'
-import AnimatedStripes from './mui/AnimatedStripes'
 
-// createRoot(document.getElementById('root')!).render(
-//   <StrictMode>
-//     <App />
-//   </StrictMode>,
-// )
-
-// createRoot(document.getElementById('root')!).render(
-//   <StrictMode>
-//     <PulsePieChart />
-//   </StrictMode>
-// )
 
 createRoot(document.querySelector("#root")!).render(
   <StrictMode>
     <StyledEngineProvider injectFirst>
     <PulsePieChart />
-    <AnimatedStripes />
     </StyledEngineProvider>
   </StrictMode>
 );

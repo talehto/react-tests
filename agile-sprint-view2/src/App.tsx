b5423ae508a92015c5af62e58c5ae93b22a4ad76
Tarmo Lehto <tarmoejlehto@gmail.com>
2025-03-06 20:09:44 +0200

    Refactored size of the Grid container which contains BackgroundColumn components

--- agile-sprint-view2/src/App.tsx
+++ agile-sprint-view2/src/App.tsx
@@ -17,13 +17,13 @@ const App: React.FC = () => {
       <CssBaseline />
       <Box sx={{ display: 'flex', flexDirection: 'column', flexGrow: 1, width: "100vw", height: "100vh", bgcolor: 'background.default' }}>
       <CustomAppBar title="Kanban board" sx={{ width: "98vw",  mt: 2 }} onButtonClick={handleButtonClick} />
-        <Grid container sx={{ flexGrow: 1 }}>
+        <Grid container sx={{ width: "70vw", height: "100vh", flexGrow: 1, margin: 'auto' }}>
           <BackgroundColumn title="Todo" size={{ xs: 4, md: 4 }} 
-            grid_sx_args={{ pl: 14, pr: 0, pt: 2, pb: 2 }} card_sx_args={{ mt: 2 }} />
+            grid_sx_args={{ pl: 2, pr: 2, pt: 2, pb: 2 }} card_sx_args={{ mt: 2 }} />
           <BackgroundColumn title="In Progress" size={{ xs: 4, md: 4 }} 
-            grid_sx_args={{ pl: 7, pr: 7, pt: 2, pb: 2 }} card_sx_args={{ mt: 2 }}  />
+            grid_sx_args={{ pl: 2, pr: 2, pt: 2, pb: 2 }} card_sx_args={{ mt: 2 }}  />
           <BackgroundColumn title="Done" size={{ xs: 4, md: 4 }} 
-            grid_sx_args={{ pl: 0, pr: 14, pt: 2, pb: 2 }} card_sx_args={{ mt: 2 }} />
+            grid_sx_args={{ pl: 2, pr: 2, pt: 2, pb: 2 }} card_sx_args={{ mt: 2 }} />
         </Grid>
       </Box>
     </ThemeProvider>

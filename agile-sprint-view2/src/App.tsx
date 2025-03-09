4aadb184a0a3d9314031d2c7f92c1eff104b9e1b
Tarmo Lehto <tarmoejlehto@gmail.com>
2025-03-09 21:35:04 +0200

    Color changes to the Accordion

--- agile-sprint-view2/src/App.tsx
+++ agile-sprint-view2/src/App.tsx
@@ -21,6 +21,7 @@ const App: React.FC = () => {
   const handleCloseStoryButtonClick = () => {
     setStories([...stories, storyTitle]);
     setOpenAddStoryDialog(false);
+    setStoryTitle('');
   };
 
   const handleCancelStoryButtonClick = () => {
@@ -44,28 +45,33 @@ const App: React.FC = () => {
       <Box sx={{ display: 'flex', flexDirection: 'column', flexGrow: 1, width: "100vw", height: "100vh", bgcolor: 'background.default' }}>
       <CustomAppBar title="Kanban board" sx={{ mt: 2, mx: 2, width: "calc(100% - 32px)" }} onButtonClick={handleAddStoryButtonClick} />
       {/* See doc for a "margin: auto" in below Grig component. https://dev.to/ritikaagrawal08/all-about-margin-auto-in-css-centering-and-more-2b2g */}
-      {stories.map((story, index) => (
-          <Accordion>
-          <AccordionSummary
-            expandIcon={<ExpandMoreIcon />}
-            aria-controls="panel1-content"
-            id={`story-${index}`}
-          >
-            <Typography component="span">{story}</Typography>
-          </AccordionSummary>
-        </Accordion>
-        ))}
-        <Grid container sx={{ width: "70vw", height: "100vh", flexGrow: 1, margin: 'auto'}}>
-        {columns.map((column, index) => (
-            <BackgroundColumn
-              key={index}
-              title={column.title}
-              size={column.size}
-              grid_sx_args={{ pl: 2, pr: 2, pt: 2, pb: 2 }}
-              card_sx_args={{ mt: 2 }}
-            />
-          ))}
-        </Grid>
+        {/* <Grid container sx={{ width: "70vw", height: "100vh", flexGrow: 1, margin: 'auto'}}> */}
+        <Box sx={{ position: 'relative', flexGrow: 1, width: "70vw", height: "100vh", margin: 'auto' }}>
+          <Grid container sx={{ width: "70vw", height: "85vh", flexGrow: 1, margin: 'auto', zIndex: 1 }}>
+          {columns.map((column, index) => (
+              <BackgroundColumn
+                key={index}
+                title={column.title}
+                size={column.size}
+                grid_sx_args={{ pl: 2, pr: 2, pt: 2, pb: 2 }}
+                card_sx_args={{ mt: 2 }}
+              />
+            ))}
+          </Grid>
+          <Box sx={{ position: 'absolute', top: 0, left: 0, width: '100%', zIndex: 2, mt: 10 }}>
+            {stories.map((story, index) => (
+              <Accordion sx={{ mt: 2, borderRadius: 2, bgcolor: 'primary.main' }} key={index}>
+              <AccordionSummary
+                expandIcon={<ExpandMoreIcon />}
+                aria-controls="panel1-content"
+                id={`story-${index}`}
+              >
+                <Typography component="span">{story}</Typography>
+              </AccordionSummary>
+            </Accordion>
+            ))}
+          </Box>
+        </Box>
       </Box>
       <StoryDialog
         openAddStoryDialog={openAddStoryDialog}
